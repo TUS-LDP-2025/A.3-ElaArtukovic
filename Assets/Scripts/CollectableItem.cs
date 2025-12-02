70a13ab991f3f946c5d36a919b58b56bf6704684
ela-artukovic-tus23 <K00299459@student.tus.ie>
2025-12-02 16:50:10 +0000

    add prefab so we can instantiate

--- Assets/Scripts/CollectableItem.cs
+++ Assets/Scripts/CollectableItem.cs
@@ -7,6 +7,7 @@ public class CollectableItem : ScriptableObject
     public int value;                      //the value
     public Color color;                    //the color
     public Sprite icon;                    //icon that will appear
+    public GameObject elementPrefab;       //prefab I will need to instantite when dropping items
 
 }                                          //to make a collectable item go to Assets - Create - Scriptable Objects at the bottom - CollectableItem 
                                            //in it I can change its data - name, value, color etc
